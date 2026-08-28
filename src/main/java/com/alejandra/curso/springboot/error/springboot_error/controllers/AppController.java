7963bebe4293a2c39489011b3a5c262d1b89ee97
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-28 16:53:27 +0200

    Creando clase DTO de Error

---
+++ src/main/java/com/alejandra/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.alejandra.curso.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class AppController {
+
+    @GetMapping("/app")
+    public String index(){
+        int value = 100/0;
+        return "ok 200";
+    }
+}
