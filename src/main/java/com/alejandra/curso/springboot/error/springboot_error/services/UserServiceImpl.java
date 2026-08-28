8cd2608b2401878a97c6437b54eebcf2fca28c84
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-28 18:07:21 +0200

    Manejando excepcion personalizada creando clases Model y Service

---
+++ src/main/java/com/alejandra/curso/springboot/error/springboot_error/services/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.alejandra.curso.springboot.error.springboot_error.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.alejandra.curso.springboot.error.springboot_error.models.domain.User;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private List<User> users;
+
+    public UserServiceImpl() {
+        this.users = new ArrayList<>();
+        users.add(new User(1L, "Alejandra", "Gonzalexx"));    
+        users.add(new User(2L, "Josefa", "Perez"));
+        users.add(new User(3L, "Pepa", "Agudelo"));
+        users.add(new User(4L, "Maria", "Pepa"));
+        users.add(new User(5L, "Yami", "Varela"));
+    }
+
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User user = null;
+        for (User u : users) {
+            if (u.getId().equals(id)) {
+                user = u;
+                break;
+            }
+        }
+        return user;
+    }
+
+}
